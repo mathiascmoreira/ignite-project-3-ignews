1b4bc60a1215755ec36cc912fb97b2ad9b82ae1d
Mathias <mathiascmoreira@gmail.com>
2021-04-04 23:53:41 -0300

    payment with stripe completed

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,26 +1,28 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
+    const [session] = useSession();
 
-    const isUserLoggedIn = true;
-
-    return isUserLoggedIn ?
+    return session ?
         (
             <button
                 type="button"
-                className={styles.signButton}>
+                className={styles.signButton}
+                onClick={() => signOut()}>
                 <FaGithub color="#04d361" />
-                Mathias Moreira
+                {session.user.name}
                 <FiX color="#737380" className={styles.closeIcon} />
             </button>
         ) :
         (
             <button
                 type="button"
-                className={styles.signButton}>
+                className={styles.signButton}
+                onClick={() => signIn('github')}>
                 <FaGithub color="#eba417" />
              Sign in with Github
             </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IZhFvHrZcpNZKLvusr3YyZY');
 
   const product = {
